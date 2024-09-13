5946c760c8665f7f94382ea08d7abcbb01f094cf
HMonique <moniquehl@outlook.com>
2024-09-13 10:58:24 -0400

    first commit

---
+++ src/test/java/com/perscholas/FileuploadExample/FileuploadExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.FileuploadExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileuploadExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
